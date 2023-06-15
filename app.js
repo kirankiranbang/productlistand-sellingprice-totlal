36059d1769febe274ea0f35311c4ae363d1692f4
kirankiranbang <105356674+kirankiranbang@users.noreply.github.com>
2023-06-15 18:27:46 +0530

    todosapp

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use('/todos', todoRoutes);
 
 
-// app.use('/todos/logindata', express.static('public/signup.html'));
+/// app.use('/todos/logindata', express.static('public/signup.html'));
 // app.use('/todos', todoRoutes);
 
 
